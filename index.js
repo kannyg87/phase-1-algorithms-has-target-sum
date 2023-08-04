bb37bc4368a543618f515d4986dbc0482906fe3f
Kanny <kanny.ghafour87@gmail.com>
2023-08-04 17:11:46 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      const sum = array[i] + array[j];
+      if (sum === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Time Complexity: O(n)
 */
 
 /* 
